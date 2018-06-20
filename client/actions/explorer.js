57cab96aaff72412a5033b294aecb7938c9b3743
asddsa356 <asddsa356@mail.ru>
2018-06-20 14:23:58 +0300

    add normal diagram, procent for table

--- client/actions/explorer.js
+++ client/actions/explorer.js
@@ -10,21 +10,46 @@ export function fetchHolders (){
          
          let resultTable =[];
          let resultDiagram=[];
-         let values = res.map(item=>item.returnValues);
+         let values = res.map(item=>item.returnValues).slice(1);
          console.log(values);
+         
          values.forEach(function(item,index){
             resultTable.push({
                address:item.to,
-               token:( item.value / 1000000000000000000)
+               token:( Math.round( item.value / 1000000000000000000))
            })
            resultDiagram.push({
             title:item.to,
-           value:10
+           value:10,
+           color:getRandomRgb(),
+           token:( Math.round( item.value / 1000000000000000000))
         })
           
         })
-         console.log(resultTable);
-dispatch({type:FETCH_HOLDERS_SUCCESS,payload:resultTable,diagram:resultDiagram})
+
+
+        const sum = _.sum(resultTable.map(item=>item.token))
+           
+        const tokens = resultTable.map((item)=>{
+            return {
+               address:item.address,
+               token:item.token,
+               percent: (item.token /sum *100).toFixed(2)
+            }
+        })
+        const diagram = resultDiagram.map((item)=>{
+            return{
+            title:item.title,
+           value:Number((item.token /sum *100).toFixed(2)),
+           color:item.color,
+
+            }
+        })
+
+
+
+         console.log(tokens);
+dispatch({type:FETCH_HOLDERS_SUCCESS,payload:tokens,diagram:diagram})
           
 }).catch(error=>{ 
                    console.log(error);
@@ -34,4 +59,11 @@ dispatch({type:FETCH_HOLDERS_SUCCESS,payload:resultTable,diagram:resultDiagram})
       
   
     }
+}
+function getRandomRgb() {
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
--- client/components/explorer/index.js
+++ client/components/explorer/index.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import styled from 'styled-components';
 import {BootstrapTable,TableHeaderColumn} from 'react-bootstrap-table';
+import _ from 'lodash';
  import PieChart from "react-svg-piechart" ;
 import {fetchHolders} from '../../actions/explorer';
 import Preloader from '../preloader/index';
@@ -25,30 +26,21 @@ import Preloader from '../preloader/index';
 		</Container>);
     }
     renderExplorer(tokenarray=[],diagram=[]){
-        // const data = [
-        //     {title: "Data 1", value: 100, color: "#22594e"},
-        //     {title: "Data 2", value: 60, color: "#2f7d6d"},
-        //     {title: "Data 3", value: 30, color: "#3da18d"},
-        //     {title: "Data 4", value: 20, color: "#69c2b0"},
-        //     {title: "Data 5", value: 10, color: "#a1d9ce"},
-        //   ]
-     
+      
+        
         return(
             
             <Fragment>
                 <DiagramContainer>
         <PieChart
         data={diagram}
-    
-        // If you need expand on hover (or touch) effect
         expandOnHover
-        // If you need custom behavior when sector is hovered (or touched)
         />
         </DiagramContainer>
         <BootstrapTable data={ tokenarray}>
-        <TableHeaderColumn dataField='address' width="450px" isKey>Address</TableHeaderColumn>
+        <TableHeaderColumn dataField='address'  isKey>Address</TableHeaderColumn>
         <TableHeaderColumn dataField='token'>Token</TableHeaderColumn>
-        <TableHeaderColumn dataField='tokenprocent'>Percent</TableHeaderColumn>
+        <TableHeaderColumn dataField='percent'>Percent</TableHeaderColumn>
       </BootstrapTable>
       </Fragment>);
     }
